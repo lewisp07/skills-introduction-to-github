782886366673ae4e3e0b454f7fe26b9c8b24aaa7
lewisp07 <lewisparry07@icloud.com>
2025-10-07 15:15:05 +0100

    Add files via upload

---
+++ week 1 new.py
@@ -0,0 +1,17 @@
+print("hello world")
+
+student_name = "lewis"
+
+student_age = 18
+
+print("student name is", student_name)
+print("student's age is", student_age)
+
+num1 = 15
+num2 = 10
+total = num1 + num2
+print("the total is", total)
+
+git ad week_1_new.py
+git commit -m "feat: complete week 1 lab exercises"
+git push
